46f590d7a6ca28478302196eb292746fc3e21c3b
impactaly <impactaky@users.noreply.github.com>
2025-11-20 16:37:10 +0900

    All CI checks pass now. The warning about "exports" is just a warning and won't fail CI.

--- tests/api_client_test.ts
+++ tests/api_client_test.ts
@@ -1,4 +1,4 @@
-import { assertEquals } from "jsr:@std/assert";
+import { assertEquals } from "@std/assert";
 import { ApiClient } from "../src/api/client.ts";
 
 Deno.test("ApiClient - constructor strips trailing slash", () => {
